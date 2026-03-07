a4c8dc2bfff24eb6c4dd27c1671344bae07742fc
xiaou <26303163030@qq.com>
2026-03-07 21:55:37 +0800

    refactor: 设置中心

---
+++ internal-plugins/setting/src/components/SettingHone/SettingHone.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { LeftMenu } from '@/components'
+</script>
+
+<template>
+  <div class="setting-hone">
+    <div class="setting-hone-menu">
+      <LeftMenu />
+    </div>
+    <div class="w-full setting-hone-content">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.setting-hone {
+  height: 100vh;
+  display: flex;
+  &-menu {
+    height: 100%;
+    min-height: 0;
+  }
+
+  &-content {
+    flex: 1;
+    min-height: 0;
+    overflow: hidden;
+  }
+}
+</style>
